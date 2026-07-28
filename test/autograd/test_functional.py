f9c3798f7d01f77d1335364de9f25a4c9a7c1169
Piotr Bielak <piotr.bielak@intel.com>
2026-07-28 14:22:29 +0000

    Enable `autograd/test_functional.py` on XPU

--- test/autograd/test_functional.py
+++ test/autograd/test_functional.py
@@ -1748,7 +1748,7 @@ class TestAutogradFunctionalDevice(TestCase):
 
 
 instantiate_parametrized_tests(TestAutogradFunctional)
-instantiate_device_type_tests(TestAutogradFunctionalDevice, globals())
+instantiate_device_type_tests(TestAutogradFunctionalDevice, globals(), allow_xpu=True)
 
 
 if __name__ == "__main__":
